06584ad3f0143312fc36e976950f6cf6f755c606
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-01-21 16:14:35 +0300

    HTTP Response Headers with filter

---
+++ BearerAuth.API/BearerAuth.API/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BearerAuth.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Login(string userName, string password)
+        {
+            if (userName == "turkay" && password == "123")
+            {
+                var claims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Email,"turkay.urkmez@dinamikzihin.com"),
+                    new Claim(ClaimTypes.Role,"Admin")
+                };
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Bu gizli bir cumle"));
+                var cryptoCredential = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                    issuer: "api.halkbank.com.tr",
+                    audience: "client.halkbank.com.tr",
+                    claims: claims,
+                    notBefore: DateTime.Now,
+                    expires: DateTime.Now.AddMinutes(60),
+                    signingCredentials: cryptoCredential
+                    );
+
+                return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+
+            }
+            return BadRequest();
+
+        }
+    }
+}
---
+++ BearerAuth.API/BearerAuth.API/Program.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace BearerAuth.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                            .AddJwtBearer(config =>
+                            {
+                                config.TokenValidationParameters = new TokenValidationParameters()
+                                {
+                                    ValidateAudience = false,
+                                    ValidateIssuer = false,
+                                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Bu gizli bir cumle")),
+                                    ValidIssuer = "api.halkbank.com.tr",
+                                    ValidAudience = "client.halkbank.com.tr", //ValidateAudience ve ValidateIssuer değerleri true ise ValidIssuer ve ValidAudience'i belirtmek zorundasınız,
+                                    ValidateIssuerSigningKey = true
+                                };
+                            });
+
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.Use(async (context, next) =>
+            {
+                if (!context.Response.Headers.ContainsKey("X-Frame-Options"))
+                {
+                    context.Response.Headers.Add("X-Frame-Options", "DENY");
+                    //https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html
+                }
+                await next();
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
